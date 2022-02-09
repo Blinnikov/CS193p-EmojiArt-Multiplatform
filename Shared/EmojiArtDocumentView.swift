7f0203056a665a86337292b6f67a6820b784cc80
Igor Blinnikov <igor.blinnikov@gmail.com>
2022-02-09 14:26:30 +0300

    feat(emojiart): lecture 16 - first run version

--- Shared/EmojiArtDocumentView.swift
+++ Shared/EmojiArtDocumentView.swift
@@ -135,7 +135,7 @@ struct EmojiArtDocumentView: View {
   
   private func handlePickedBackgroundImage(_ image: UIImage?) {
     autozoom = true
-    if let imageData = image?.jpegData(compressionQuality: 1.0) {
+    if let imageData = image?.imageData {
       document.setBackground(.imageData(imageData), undoManager: undoManager)
     }
     backgroundPicker = nil
@@ -151,9 +151,9 @@ struct EmojiArtDocumentView: View {
   
   private func pasteBackground() {
     autozoom = true
-    if let imageData = UIPasteboard.general.image?.jpegData(compressionQuality: 1.0) {
+    if let imageData = Pasteboard.imageData {
       document.setBackground(.imageData(imageData), undoManager: undoManager)
-    } else if let url = UIPasteboard.general.url?.imageURL {
+    } else if let url = Pasteboard.imageURL {
       document.setBackground(.url(url), undoManager: undoManager)
     } else {
       alertToShow = IdentifiableAlert(
@@ -182,6 +182,7 @@ struct EmojiArtDocumentView: View {
       autozoom = true
       document.setBackground(.url(url.imageURL), undoManager: undoManager)
     }
+    #if os(iOS)
     if !found {
       found = providers.loadObjects(ofType: UIImage.self) { image in
         if let data = image.jpegData(compressionQuality: 1.0) {
@@ -190,6 +191,7 @@ struct EmojiArtDocumentView: View {
         }
       }
     }
+    #endif
     if !found {
       found = providers.loadObjects(ofType: String.self) { string in
         if let emoji = string.first, emoji.isEmoji {
